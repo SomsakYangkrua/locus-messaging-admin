fd65668961cffa7144a83308f65a80bc0d611cdd
Somsak Yangkrua <somsak.yangkrua@locus.co.th>
2021-02-03 17:32:29 +0700

    set init value of system configuration

--- src/app/systems/edit-systemconfig/edit-systemconfig.component.ts
+++ src/app/systems/edit-systemconfig/edit-systemconfig.component.ts
@@ -1,10 +1,8 @@
 import { SystemsconfigService } from './../systemsconfig.service';
 import { Systemconfig } from './../systemconfig.model';
 import { Component, OnInit } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { FormControl, FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-edit-systemconfig',
@@ -13,78 +11,6 @@ import { environment } from 'src/environments/environment';
 })
 export class EditSystemconfigComponent implements OnInit {
   systemsconfig: Systemconfig = new Systemconfig();
-  // systemsconfig = {
-  //     id: '0',
-  //     mode: 'development',
-  //     auth_key: '1234567890',
-  //     auth_Realm: 'test_dev',
-  //     admin_username: 'admin',
-  //     admin_password: '123',
-  //     db_type: 'mysql',
-  //     db_str_cnn: 'root:qazwsx@tcp(localhost:3306)/auth?charset=utf8',
-  //     in_memory_db_server: '192.168.10.188:6379',
-  //     in_memory_db_database: '8',
-  //     in_memory_db_pwd: '',
-  //     in_memory_db_expiration: '0',
-  //     in_memory_db_PoolSize: '100',
-  //     in_memory_db_Read_WriteTimeout: '15',
-  //     in_memory_db_MinIdleConns: '5',
-  //     in_memory_db_MaxConnAge: '10',
-  //     in_memory_db_PoolTimeout: '500',
-  //     host: '',
-  //     bind_tls: 'false',
-  //     key_file: '/etc/letsencrypt/live/infra-dev.locus.co.th/privkey.pem',
-  //     cert_file: '/etc/letsencrypt/live/infra-dev.locus.co.th/fullchain.pem',
-  //     udp_bind_addr: '127.0.0.1:1234',
-  //     proxyrevers_host: 'http://192.168.22.129:15000/api/v1/messenger-to-dialogflow',
-  //     webhook_url: 'https://infra-dev.locus.co.th:15000/api/v1/reverse-proxy',
-  //     webhook_channel_secret: 'c85b03b69e02cdd5b58c712b23fbe0df',
-  //     webhook_channel_token: 'xQy+RPXJ9yQqT6BAOSe7YFjhyZvuyHJtDInGcmm8OFO8LQwDytJr5elpHsyckPEiUm/HkI58/C9IWG7Afw+IgqZVW7ueVfIOSOf8EvM2x306Nvy1jcBJZmifBWFf8tEw/X97E5Ij5cOZclfL0wpeYgdB04t89/1O/w1cDnyilFU=',
-  //     webhook_session_expire_time: '600',
-  //     webhook_temp_path: './temp',
-  //     webhook_audio_type: 'wav',
-  //     webhook_audio_lang: 'th',
-  //     webhook_line_sticker_url: 'https://stickershop.line-scdn.net/stickershop/v1/sticker/@sticker-id/android/sticker.png',
-  //     webhook_keyword_mapping_topureconnect: '0:contact agent',
-  //     webhook_agent_respone_timeout: '27',
-  //     webhook_agent_respone_default_text: 'Right now agent is busy if you would like to continue please say again?',
-  //     dialogflow_projectID: 'checkorderstatus-vkvj',
-  //     dialogflow_sessionID: '',
-  //     dialogflow_AudioEncoding: '1',
-  //     dialogflow_SampleRateHertz: '16000',
-  //     dialogflow_GOOGLE_APPLICATION_CREDENTIALS: 'D:\\WorkSpace\\GoWorkspace\\src\\line-integration\\application_default_credentials.json',
-  //     facebook_messenger_AccessToken: 'EAAGQEDPdYzwBABRZAqS31MMPiAYW4SaZBD5RRwdBx2KKCG7qBQCPbjlU2Pb3K3uUTGTImaL4oZC6xwGal94GjdljYZB9ufwm9YsAQokyw2ZCH4znG6hZA3LKFwK2vN774DJr5ALLgJ4YD4lvlZCFagJoWzpUaj3jZBwNCgmoCDbFUwjSR4ubTSBz',
-  //     facebook_messenger_VerifyToken: '1748176972',
-  //     facebook_messenger_PageID: '150657113154587',
-  //     facebook_messenger_webhook_url: 'http://192.168.22.129:15000/api/v1/messenger-to-dialogflow',
-  //     whatsapp_timeout: '60',
-  //     whatsapp_mapping_backend: 'dialogflow',
-  //     iwt_application_name: 'Line-Integration',
-  //     iwt_primary_api: 'http://192.168.20.190/I3Root/Server1/websvcs/',
-  //     iwt_backup_api: 'http://192.168.20.190/I3Root/Server1/websvcs/',
-  //     iwt_cert_file: '',
-  //     iwt_skip_auto_agent_ack: '1',
-  //     iwt_username: 'cicadmin',
-  //     iwt_password: '1234',
-  //     iwt_chat_queuename: 'Marketing',
-  //     iwt_chat_message_poll_interval: '2',
-  //     iwt_ftp_protocal: 'ftp',
-  //     iwt_ftp_server: '192.168.10.101',
-  //     iwt_ftp_port: '21',
-  //     iwt_ftp_user: 'botadmin',
-  //     iwt_ftp_pwd: 'Locus@123',
-  //     iwt_ftp_path: '/',
-  //     background_queue_size: '1024',
-  //     background_worker_size: '25',
-  //     amqp_url: 'amqp://locus:Locus@123@192.168.10.188:5672/',
-  //     amqp_qname: 'test-queue',
-  //     amqp_exchange: 'events',
-  //     amqp_routing_key: 'test-queue',
-  //     amqp_content_type: 'application/json',
-  //     amqp_rpc_timeout: '5'
-  // };
-
-  //===========
   tabs = [];
   selected = new FormControl(0);
   editSystemconfigForm: FormGroup;
@@ -102,8 +28,7 @@ export class EditSystemconfigComponent implements OnInit {
 
   constructor(
     private fb: FormBuilder,
-    public httpClient: HttpClient,
-    //public systemsconfigService: SystemsconfigService
+    private systemsconfigService: SystemsconfigService
     )
   {
     this.editSystemconfigForm = this.createSystemconfigForm();
@@ -111,17 +36,11 @@ export class EditSystemconfigComponent implements OnInit {
   }
 
   ngOnInit(): void {
-    // this.systemsconfigService.getSystemConfig()
-    // .subscribe((data) => {
-    //   this.systemsconfigService = data.result[0];
-    // });
-
-    this.httpClient.get<any>(`${environment.apiUrl}/api/v1/systems/config/list`)
-    .subscribe((data)=>{
+    this.systemsconfigService.getSystemConfig()
+    .subscribe((data) => {
       this.systemsconfig = data.result[0];
       this.editSystemconfigForm = this.createSystemconfigForm();
     });
-
   }
 
   onSubmit() {
@@ -136,7 +55,7 @@ export class EditSystemconfigComponent implements OnInit {
       admin_username: [this.systemsconfig.admin_username || ''],
       admin_password: [this.systemsconfig.admin_password || ''],
 
-      db_type: [this.systemsconfig.db_type || ''],
+      db_type: [this.systemsconfig.db_type || 'mysql'],
       db_str_cnn: [this.systemsconfig.db_str_cnn || ''],
       in_memory_db_server: [this.systemsconfig.in_memory_db_server || ''],
       in_memory_db_database: [this.systemsconfig.in_memory_db_database || ''],
@@ -149,10 +68,10 @@ export class EditSystemconfigComponent implements OnInit {
       in_memory_db_PoolTimeout: [this.systemsconfig.in_memory_db_PoolTimeout || ''],
 
       host: [this.systemsconfig.host || ''],
-      bind_tls: [this.systemsconfig.bind_tls || ''],
+      bind_tls: [this.systemsconfig.bind_tls || 'false'],
       key_file: [this.systemsconfig.key_file || ''],
       cert_file: [this.systemsconfig.cert_file || ''],
-      udp_bind_addr: [this.systemsconfig.udp_bind_addr || ''],
+      udp_bind_addr: [this.systemsconfig.udp_bind_addr || '127.0.0.1:3497'],
 
       proxyrevers_host: [this.systemsconfig.proxyrevers_host || ''],
 
@@ -160,18 +79,18 @@ export class EditSystemconfigComponent implements OnInit {
       webhook_channel_secret: [this.systemsconfig.webhook_channel_secret || ''],
       webhook_channel_token: [this.systemsconfig.webhook_channel_token || ''],
       webhook_session_expire_time: [this.systemsconfig.webhook_session_expire_time || ''],
-      webhook_temp_path: [this.systemsconfig.webhook_temp_path || ''],
+      webhook_temp_path: [this.systemsconfig.webhook_temp_path || './temp'],
       webhook_audio_type: [this.systemsconfig.webhook_audio_type],
-      webhook_audio_lang: [this.systemsconfig.webhook_audio_type || ''],
+      webhook_audio_lang: [this.systemsconfig.webhook_audio_lang || 'th'],
       webhook_line_sticker_url: [this.systemsconfig.webhook_line_sticker_url || ''],
       webhook_keyword_mapping_topureconnect: [this.systemsconfig.webhook_keyword_mapping_topureconnect || ''],
-      webhook_agent_respone_timeout: [this.systemsconfig.webhook_agent_respone_timeout || ''],
-      webhook_agent_respone_default_text: [this.systemsconfig.webhook_agent_respone_timeout || ''],
+      webhook_agent_respone_timeout: [this.systemsconfig.webhook_agent_respone_timeout || '27'],
+      webhook_agent_respone_default_text: [this.systemsconfig.webhook_agent_respone_default_text || 'Right now agent is busy if you would like to continue please say again?/กรุณาสอบถามอีกครั้งคะ'],
 
       dialogflow_projectID: [this.systemsconfig.dialogflow_projectID || ''],
       dialogflow_sessionID: [this.systemsconfig.dialogflow_sessionID || ''],
-      dialogflow_AudioEncoding: [this.systemsconfig.dialogflow_sessionID || ''],
-      dialogflow_SampleRateHertz: [this.systemsconfig.dialogflow_SampleRateHertz || ''],
+      dialogflow_AudioEncoding: [this.systemsconfig.dialogflow_AudioEncoding || '1'],
+      dialogflow_SampleRateHertz: [this.systemsconfig.dialogflow_SampleRateHertz || '16000'],
       dialogflow_GOOGLE_APPLICATION_CREDENTIALS: [this.systemsconfig.dialogflow_GOOGLE_APPLICATION_CREDENTIALS || ''],
 
       facebook_messenger_AccessToken: [this.systemsconfig.facebook_messenger_AccessToken || ''],
@@ -179,30 +98,30 @@ export class EditSystemconfigComponent implements OnInit {
       facebook_messenger_PageID: [this.systemsconfig.facebook_messenger_PageID || ''],
       facebook_messenger_webhook_url: [this.systemsconfig.facebook_messenger_webhook_url || ''],
 
-      whatsapp_timeout: [this.systemsconfig.whatsapp_timeout],
-      whatsapp_mapping_backend: [this.systemsconfig.whatsapp_timeout || ''],
+      whatsapp_timeout: [this.systemsconfig.whatsapp_timeout || '60'],
+      whatsapp_mapping_backend: [this.systemsconfig.whatsapp_mapping_backend || 'dialogflow'],
 
-      iwt_application_name: [this.systemsconfig.iwt_application_name || ''],
+      iwt_application_name: [this.systemsconfig.iwt_application_name || 'Locus Messaging'],
       iwt_primary_api: [this.systemsconfig.iwt_primary_api || ''],
       iwt_backup_api: [this.systemsconfig.iwt_backup_api || ''],
       iwt_cert_file: [this.systemsconfig.iwt_cert_file || ''],
-      iwt_skip_auto_agent_ack: [this.systemsconfig.iwt_skip_auto_agent_ack || ''],
+      iwt_skip_auto_agent_ack: [this.systemsconfig.iwt_skip_auto_agent_ack || '1'],
       iwt_username: [this.systemsconfig.iwt_username || ''],
       iwt_password: [this.systemsconfig.iwt_password || ''],
       iwt_chat_queuename: [this.systemsconfig.iwt_chat_queuename || ''],
-      iwt_chat_message_poll_interval: [this.systemsconfig.iwt_chat_message_poll_interval || ''],
-      iwt_ftp_protocal: [this.systemsconfig.iwt_ftp_protocal || ''],
+      iwt_chat_message_poll_interval: [this.systemsconfig.iwt_chat_message_poll_interval || '2'],
+      iwt_ftp_protocal: [this.systemsconfig.iwt_ftp_protocal || 'ftp'],
       iwt_ftp_server: [this.systemsconfig.iwt_ftp_server || ''],
-      iwt_ftp_port: [this.systemsconfig.iwt_ftp_port || ''],
+      iwt_ftp_port: [this.systemsconfig.iwt_ftp_port || '21'],
       iwt_ftp_user: [this.systemsconfig.iwt_ftp_user || ''],
       iwt_ftp_pwd: [this.systemsconfig.iwt_ftp_pwd || ''],
-      iwt_ftp_path: [this.systemsconfig.iwt_ftp_path || ''],
+      iwt_ftp_path: [this.systemsconfig.iwt_ftp_path || '/'],
 
       amqp_url: [this.systemsconfig.amqp_url  || ''],
       amqp_qname: [this.systemsconfig.amqp_qname || ''],
       amqp_exchange: [this.systemsconfig.amqp_exchange || ''],
       amqp_routing_key: [this.systemsconfig.amqp_routing_key || ''],
-      amqp_content_type: [this.systemsconfig.amqp_content_type || ''],
+      amqp_content_type: [this.systemsconfig.amqp_content_type || 'application/json'],
       amqp_rpc_timeout: [this.systemsconfig.amqp_rpc_timeout || ''],
       background_queue_size: [this.systemsconfig.background_queue_size || ''],
       background_worker_size: [this.systemsconfig.background_worker_size || ''],
--- src/app/systems/systemsconfig.service.ts
+++ src/app/systems/systemsconfig.service.ts
@@ -1,152 +1,24 @@
 import { Systemconfig } from './systemconfig.model';
 import { Injectable } from '@angular/core';
-import { BehaviorSubject, Observable } from 'rxjs';
-import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
 import { environment } from '../../environments/environment';
 
-@Injectable()
+@Injectable({
+  providedIn: 'root'
+})
+
 export class SystemsconfigService {
-  //private readonly API_URL = 'assets/data/department.json';
-  isTblLoading = true;
-  dataChange: BehaviorSubject<Systemconfig[]> = new BehaviorSubject<Systemconfig[]>([]);
-  // Temporarily stores data from dialogs
-  dialogData: any;
   constructor(private httpClient: HttpClient) {}
-  get data(): Systemconfig[] {
-    return this.dataChange.value;
-  }
-  getDialogData() {
-    return this.dialogData;
-  }
-  /** CRUD METHODS */
-  // getSystemConfig(): void {
-  //   let mapConfig = new Map();
-  //   this.httpClient.get<any>(`${environment.apiUrl}/api/v1/systems/config/list`).subscribe(
-  //     (data) => {
-  //       var allsystemconfig: Allsystemconfig;
-  //       var systemConfig = new Systemconfig();
-  //       allsystemconfig = data.result;
-  //       systemConfig.mode = data.mode;
-
-  //       //Authorization
-  //       mapConfig = this.loadMapConfig(mapConfig, allsystemconfig.Authorization);
-  //       systemConfig.auth_key = mapConfig.get("auth_key");
-  //       systemConfig.auth_Realm = mapConfig.get("auth_Realm");
-  //       systemConfig.admin_username = mapConfig.get("admin_username");
-  //       systemConfig.admin_username = mapConfig.get("admin_password");
-  //       //Database
-  //       mapConfig = this.loadMapConfig(mapConfig, allsystemconfig.Database);
-  //       systemConfig.db_type = mapConfig.get("db_type");
-  //       systemConfig.db_str_cnn = mapConfig.get("db_str_cnn");
-  //       systemConfig.in_memory_db_server = mapConfig.get("in_memory_db.server");
-  //       systemConfig.in_memory_db_database = mapConfig.get("in_memory_db.database");
-  //       systemConfig.in_memory_db_pwd = mapConfig.get("in_memory_db.pwd");
-  //       systemConfig.in_memory_db_expiration = mapConfig.get("in_memory_db.expiration");
-  //       systemConfig.in_memory_db_PoolSize = mapConfig.get("in_memory_db.PoolSize");
-  //       systemConfig.in_memory_db_Read_WriteTimeout = mapConfig.get("in_memory_db.Read_WriteTimeout");
-  //       systemConfig.in_memory_db_MinIdleConns = mapConfig.get("in_memory_db.MinIdleConns");
-  //       systemConfig.in_memory_db_MaxConnAge = mapConfig.get("in_memory_db.MaxConnAge");
-  //       systemConfig.in_memory_db_PoolTimeout = mapConfig.get("in_memory_db.PoolTimeout");
-
-  //       //LocalHTTPServer
-  //       mapConfig = this.loadMapConfig(mapConfig, allsystemconfig.LocalHTTPServer);
-  //       systemConfig.host = mapConfig.get("host");
-  //       systemConfig.bind_tls = mapConfig.get("bind_tls");
-  //       systemConfig.key_file = mapConfig.get("key_file");
-  //       systemConfig.cert_file = mapConfig.get("cert_file");
-
-  //       //LocalUDPServer
-  //       mapConfig = this.loadMapConfig(mapConfig, allsystemconfig.LocalUDPServer);
-  //       systemConfig.udp_bind_addr = mapConfig.get("udp.bind_addr");
-  //       //ProxyReversToDev
-  //       mapConfig = this.loadMapConfig(mapConfig, allsystemconfig.ProxyReversToDev);
-  //       systemConfig.proxyrevers_host = mapConfig.get("proxyrevers.host");
-  //       //InterfaceLine
-  //       mapConfig = this.loadMapConfig(mapConfig, allsystemconfig.InterfaceLine);
-  //       systemConfig.webhook_url = mapConfig.get("webhook.url");
-  //       systemConfig.webhook_channel_secret = mapConfig.get("webhook.channel_secret");
-  //       systemConfig.webhook_channel_token = mapConfig.get("webhook.channel_token");
-  //       systemConfig.webhook_session_expire_time = mapConfig.get("webhook.session_expire_time");
-  //       systemConfig.webhook_temp_path = mapConfig.get("webhook.temp_path");
-  //       systemConfig.webhook_audio_type = mapConfig.get("webhook.audio_type");
-  //       systemConfig.webhook_audio_lang = mapConfig.get("webhook.audio_lang");
-  //       systemConfig.webhook_line_sticker_url = mapConfig.get("webhook.line_sticker_url");
-  //       systemConfig.webhook_keyword_mapping_topureconnect = mapConfig.get("webhook.keyword_mapping_topureconnect");
-  //       systemConfig.webhook_agent_respone_timeout = mapConfig.get("webhook.agent_respone_timeout");
-  //       systemConfig.webhook_agent_respone_default_text = mapConfig.get("webhook.agent_respone_default_text");
-  //       //InterfaceDialogFlow
-  //       mapConfig = this.loadMapConfig(mapConfig, allsystemconfig.InterfaceLine);
-  //       systemConfig.dialogflow_projectID = mapConfig.get("dialogflow.projectID");
-  //       systemConfig.dialogflow_sessionID = mapConfig.get("dialogflow.sessionID");
-  //       systemConfig.dialogflow_sessionID = mapConfig.get("dialogflow.AudioEncoding");
-  //       systemConfig.dialogflow_SampleRateHertz = mapConfig.get("dialogflow.SampleRateHertz");
-  //       systemConfig.dialogflow_GOOGLE_APPLICATION_CREDENTIALS = mapConfig.get("dialogflow.GOOGLE_APPLICATION_CREDENTIALS");
-
-  //       //InterfaceMessenger
-  //       mapConfig = this.loadMapConfig(mapConfig, allsystemconfig.InterfaceMessenger);
-  //       systemConfig.facebook_messenger_AccessToken = mapConfig.get("facebook.messenger.AccessToken");
-  //       systemConfig.facebook_messenger_VerifyToken = mapConfig.get("facebook.messenger.VerifyToken");
-  //       systemConfig.facebook_messenger_PageID = mapConfig.get("facebook.messenger.PageID");
-  //       systemConfig.facebook_messenger_webhook_url = mapConfig.get("facebook.messenger.webhook_url");
-  //       //InterfaceWhatsapp
-  //       mapConfig = this.loadMapConfig(mapConfig, allsystemconfig.InterfaceWhatsapp);
-  //       systemConfig.whatsapp_timeout = mapConfig.get("whatsapp.timeout");
-  //       systemConfig.whatsapp_mapping_backend = mapConfig.get("whatsapp.mapping_backend");
-  //       //InterfacePureConnect
-  //       mapConfig = this.loadMapConfig(mapConfig, allsystemconfig.InterfacePureConnect);
-  //       systemConfig.iwt_application_name = mapConfig.get("iwt.application.name");
-  //       systemConfig.iwt_primary_api = mapConfig.get("iwt.primary_api");
-  //       systemConfig.iwt_backup_api = mapConfig.get("iwt.backup_api");
-  //       systemConfig.iwt_cert_file = mapConfig.get("iwt.cert_file");
-  //       systemConfig.iwt_skip_auto_agent_ack = mapConfig.get("iwt.skip_auto_agent_ack");
-  //       systemConfig.iwt_username = mapConfig.get("iwt.username");
-  //       systemConfig.iwt_password = mapConfig.get("iwt.password");
-  //       systemConfig.iwt_chat_queuename = mapConfig.get("iwt.chat.queuename");
-  //       systemConfig.iwt_chat_queuename = mapConfig.get("iwt.chat.message.poll_interval");
-  //       systemConfig.iwt_ftp_protocal = mapConfig.get("iwt.ftp_protocal");
-  //       systemConfig.iwt_ftp_server = mapConfig.get("iwt.ftp_server");
-  //       systemConfig.iwt_ftp_port = mapConfig.get("iwt.ftp_port");
-  //       systemConfig.iwt_ftp_user = mapConfig.get("iwt.ftp_user");
-  //       systemConfig.iwt_ftp_pwd = mapConfig.get("iwt.ftp_pwd");
-  //       systemConfig.iwt_ftp_path = mapConfig.get("iwt.ftp_path");
-
-  //       //InterfaceRabbitMQ
-  //       mapConfig = this.loadMapConfig(mapConfig, allsystemconfig.InterfaceRabbitMQ);
-  //       systemConfig.amqp_url = mapConfig.get("amqp.url");
-  //       systemConfig.amqp_qname = mapConfig.get("amqp.qname");
-  //       systemConfig.amqp_exchange = mapConfig.get("amqp.exchange");
-  //       systemConfig.amqp_routing_key = mapConfig.get("amqp.routing_key");
-  //       systemConfig.amqp_content_type = mapConfig.get("amqp.content_type");
-  //       systemConfig.amqp_rpc_timeout = mapConfig.get("amqp.rpc_timeout");
-  //       //AsynchronizeWorker
-  //       mapConfig = this.loadMapConfig(mapConfig, allsystemconfig.AsynchronizeWorker);
-  //       systemConfig.background_queue_size = mapConfig.get("background.queue.size");
-  //       systemConfig.background_worker_size = mapConfig.get("background.worker.size");
-
-  //       this.isTblLoading = false;
-  //       this.dataChange.next(Array(systemConfig));
-  //     },
-  //     (error: HttpErrorResponse) => {
-  //       this.isTblLoading = false;
-  //       console.log(error.name + ' ' + error.message);
-  //     }
-  //   );
-  // }
 
   getSystemConfig(): Observable<any> {
     return this.httpClient.get<any>(`${environment.apiUrl}/api/v1/systems/config/list`);
   }
 
   updateSystemConfig(systemConfig: Systemconfig): void {
-    this.dialogData = systemConfig;
+    //192.168.22.129:15000/api/v1/systems/config/update
+    //this.dialogData = systemConfig;
   }
-
-  // loadMapConfig( mapConfig: Map<string,string>, params: Parameter[]): any {
-  //   for (var param of params){
-  //     mapConfig.set(param.name, param.value);
-  //   }
-  //    return mapConfig;
-  // }
 }
 
 
